7e3ccce665ff31d043c64ca9dfb54698c2a7a360
Julian Rivera <julianarevircoins@gmail.com>
2023-11-11 19:52:57 -0500

    env

--- routes/tripadvisor.js
+++ routes/tripadvisor.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 const axios = require("axios");
 
-const fetch = require("node-fetch");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   axios
