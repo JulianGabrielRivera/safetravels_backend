5653ec33ee4a54f89da82d4a1b031d4513924fc4
Julian Rivera <julianarevircoins@gmail.com>
2023-11-19 01:45:44 -0500

    stripe

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const accountSid = process.env.TWILIO_ACC_SID;
+
+const authToken = process.env.TWILIO_ACC_TOKEN;
+
+const client = require("twilio")(accountSid, authToken);
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_KEY);
+const nodemailer = require("nodemailer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/send-message", async (req, res) => {
+  try {
+    const message = await client.messages.create({
+      body: "Hello from Node",
+      to: "+13212638191",
+      from: "+18556242805",
+    });
+    console.log(message);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/email-promo", (req, res) => {
+  const msg = {
+    to: "juliangabrielriveradev@gmail.com", // Change to your recipient
+    from: "juliangabrielriveradev@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log(msg);
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 module.exports = router;
--- routes/stripe.js
+++ routes/stripe.js
@@ -4,63 +4,81 @@ const axios = require("axios");
 
 const stripe = require("stripe")(`${process.env.STRIPE_KEY}`);
 const endpointSecret = "whsec_Xw9HK00uvxCL595WC5Y8YZKskY9ckuAS";
-
+const fulfillOrder = (lineItems) => {
+  // TODO: fill me in
+  console.log("Fulfilling order", lineItems);
+};
 router.post("/stripe", async (req, res) => {
   try {
+    const product = await stripe.products.retrieve("prod_P28zCO9jHcC8no");
     const session = await stripe.checkout.sessions.create({
       success_url: "https://example.com/success",
-
-      line_items: [{ price: "price_1OBA5TFNeaqqGqZ9ztemS7yl", quantity: 1 }],
-      mode: "subscription",
-      //   invoice_creation: true,
+      line_items: [{ price: "price_1OE4hFFNeaqqGqZ9ogLoxS7S", quantity: 1 }],
+      mode: "payment",
     });
 
-    const payload = req.body;
-    console.log(payload);
-    // const session2 = await stripe.checkout.sessions.retrieve(`${session.id}`);
-    // console.log(session2, "sesh");
-    // const paymentIntent = await stripe.paymentIntents.create({
-    //   amount: 0.01,
-    //   currency: "usd",
-    //   payment_method_types: ["card"],
-    //   description: "Thanks for your purchase!",
-    //   receipt_email: "juliangabrielriveradev@gmail.com",
-    // });
-    // console.log(paymentIntent);
-    // const invoice = await stripe.invoices.sendInvoice(`${session.id}`);
-    // console.log(invoice);
     console.log(session);
-    res.json(session.url);
+    res.json(session);
   } catch (err) {
     console.log(err);
   }
 });
 
-router.post("/webhook", async (req, res) => {
-  const payload = req.body;
-  console.log(payload);
-
+router.post("/webhook", async (request, response) => {
+  const payload = request.body;
   const sig = request.headers["stripe-signature"];
 
   let event;
 
   try {
     event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-    if (event.type === "checkout.session.completed") {
-      console.log("hey");
-      // Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
-      const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
-        event.data.object.id,
-        {
-          expand: ["line_items"],
-        }
-      );
-
-      res.status(200).end();
-    }
   } catch (err) {
-    return res.status(400).send(`Webhook Error: ${err.message}`);
+    return response.status(400).send(`Webhook Error: ${err.message}`);
   }
+
+  // Handle the checkout.session.completed event
+  if (event.type === "checkout.session.completed") {
+    // Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ["line_items"],
+      }
+    );
+    const lineItems = sessionWithLineItems.line_items;
+
+    // Fulfill the purchase...
+    fulfillOrder(lineItems);
+  }
+
+  response.status(200).end();
 });
+
+// router.post("/webhook", async (req, res) => {
+//   const payload = req.body;
+//   console.log(payload);
+
+//   const sig = request.headers["stripe-signature"];
+
+//   let event;
+
+//   try {
+//     event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+//     if (event.type === "checkout.session.completed") {
+//       console.log("hey");
+//       // Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+//       const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+//         event.data.object.id,
+//         {
+//           expand: ["line_items"],
+//         }
+//       );
+
+//       res.status(200).end();
+//     }
+//   } catch (err) {
+//     return res.status(400).send(`Webhook Error: ${err.message}`);
+//   }
+// });
 
 module.exports = router;
